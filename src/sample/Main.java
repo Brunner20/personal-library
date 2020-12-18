af377b61452690a9f8105369b494ccb8a4bc7a9a
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2020-12-18 17:41:45 +0300

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import Entity.File;
+import Entity.FileCollection;
 import Service.FileCollectionService;
 import Service.FileService;
 import javafx.application.Application;
@@ -7,14 +9,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.MainWindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FileCollectionService collectionService = new FileCollectionService();
-        FileService fileService = new FileService();
+        List<FileCollection> collections = new ArrayList<>();
+        FileCollectionService collectionService = new FileCollectionService(collections);
+
+        List<File> files =new ArrayList<>();
+        FileService fileService = new FileService(files);
         MainWindow window = new MainWindow(collectionService,fileService);
-        Scene scene =new Scene(window.getAnchorPane(),950,600);
+        Scene scene =new Scene(window.getAnchorPane(),950,1000);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
