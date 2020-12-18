d3a97a4ea1d9e99255ed27a9fa9a5ce8b60c04e9
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2020-12-18 17:40:55 +0300

    Update FileService.java

--- src/Service/FileService.java
+++ src/Service/FileService.java
@@ -3,24 +3,42 @@ package Service;
 import Entity.File;
 import Entity.User;
 
+import java.util.List;
+
 public class FileService {
 
+    private List<File> files;
+
+    public FileService(List<File> files){
+        this.files=files;
+    }
+
     public boolean uploadFile(File file){
-        return false;
+        files.add(file);
+        return true;
     }
     public boolean downloadFile(File file){
         return false;
     }
 
     public File getFileByAuthor(User author){
-        return new File();
+
+        for (File file:files)
+            if(file.getAuthor().equals(author))
+                return file;
+
+        return null;
     }
 
     public File getFileByName(String name ){
-        return new File();
+        for (File file:files)
+            if(file.getName().equals(name))
+                return file;
+
+        return null;
     }
 
     public boolean openFile(File file){
-        return false;
+        return true;
     }
 }
