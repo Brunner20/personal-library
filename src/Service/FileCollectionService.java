063874447a047d68c2b348a94e5d5aab62a101ed
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2020-12-18 17:40:27 +0300

    Update FileCollectionService.java

--- src/Service/FileCollectionService.java
+++ src/Service/FileCollectionService.java
@@ -3,24 +3,52 @@ package Service;
 import Entity.File;
 import Entity.FileCollection;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FileCollectionService {
+    
+    private List<FileCollection> allFileCollection = new ArrayList<>();
+
+    public  FileCollectionService(List<FileCollection> list){
+        this.allFileCollection=list;
+    }
 
     public boolean addFileToCollection(FileCollection fileCollection, File file){
+        int index = allFileCollection.indexOf(fileCollection);
+        if(index>=0)
+        {
+            allFileCollection.get(index).addFile(file);
+            return true;
+        }
         return false;
     }
 
     public boolean removeFileFromCollection(FileCollection fileCollection,File file){
+        int index = allFileCollection.indexOf(fileCollection);
+        if(index>=0)
+        {
+            allFileCollection.get(index).removeFile(file);
+            return true;
+        }
         return false;
     }
 
     public FileCollection getCollectionByName(String name){
-        return new FileCollection();
+        for(FileCollection collection:allFileCollection)
+            if(collection.getNames().equals(name))
+                return collection;
+
+        return null;
     }
 
     public boolean saveCollection(FileCollection collection){
-        return false;
+        allFileCollection.add(collection);
+        return true;
     }
     public boolean removeCollection(FileCollection fileCollection){
-        return false;
+        allFileCollection.remove(fileCollection);
+        return true;
     }
 }
